a2d6d910da0ec8d0161bfa3982b47fa303cfce39
He Deng <he.deng1230@gmail.com>
2025-10-25 10:06:31 +0200

    Container test

--- src/Board/Game_Board.js
+++ src/Board/Game_Board.js
@@ -1,62 +1,28 @@
 import Logic_Board from "./Logic_Board.js"
-import { Vertex_Graphic } from "./Graphic_Vertex.js";
-import { Square_Graphic } from "./Graphic_Square.js";
+// import Graphic_Board from "./Graphic_Board.js"
 
-export default class Game_Board extends Phaser.GameObjects.Image{
+export default class Game_Board extends Phaser.GameObjects.Graphics{
     constructor(scene,x,y,texture,cellSize){
-        super(scene,((x-2)*cellSize)/2,((y-2)*cellSize)/2,texture[1]);
-
-        this.texture = texture
-        this.cellSize = cellSize;
-        this.GRAPHIC = scene.add.graphics({ lineStyle: { width: 1, color: 0x00ff00 } });
+        super(scene,x,y);
         this.logic = new Logic_Board(x,y);
-
-
-        this.setAlpha(0.1);
-        this.setDisplaySize(x*cellSize,y*cellSize)
         //Dejar la grafica directamente aqui
-        this.graphic = []
-        this.Graphic_initialize(scene,x,y,this.logic);
+        this.matrix = []
+        this.graphic = new Graphic_Board(scene,x,y,this.logic,texture,cellSize);
 
         scene.add.existing(this);
     }
 
+    /**
+     * 
+     * @param {Logic_Board} logic 
+     * @param {Graphic_Board} graphic 
+     */
+
     render(){
-        this.graphic.forEach((row) =>{
-            row.forEach(point =>{
-                if(point instanceof Square_Graphic){
-                    point.render();
-                }
-                if(point instanceof Vertex_Graphic){
-                    point.render();
-                }
-            })
-        })
-
-        console.table(this.graphic)
+        this.graphic.render();
     }
 
-    Graphic_initialize(scene,x,y,logic){
-
-        for(let i = 0; i < x; ++i){
-            this.graphic[i] = [];
-            for(let j = 0; j < y; ++j){
-                this.Create_Graphic_Point(scene,i,j);
-            }
-        }
-        console.table(this.graphic);
+    addGraphic(texture){
+        this.graphic.add(texture);
     }
-
-    Create_Graphic_Point(scene,x,y){
-        if((x%2 == 0) && (y%2 == 0)){
-            this.graphic[x][y] = new Vertex_Graphic(scene,this.GRAPHIC,this.cellSize,this.logic.matrix[x][y]);
-        }
-        else if((x%2 == 1) && (y%2 == 1)){
-            this.graphic[x][y] = new Square_Graphic(scene,this.logic.matrix[x][y],this.texture[0],40);
-        }
-        else {
-            this.graphic[x][y] = null;
-        }   
-    }
-
 }
--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 import {Game_Screen} from "./Scene/Game_Screen.js";
 import { Menu } from "./Scene/Menu.js";	
 import { Submarine_View } from "./Scene/Submarine_View.js";
+import Container_Scene from "./Container_test/Container_Scene.js";
 /**
  * Inicio del juego en Phaser. Creamos el archivo de configuración del juego y creamos
  * la clase Game de Phaser, encargada de crear e iniciar el juego.
@@ -14,7 +15,7 @@ let config = {
 	scale: {
 		autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
 	},
-	scene: [Menu, Submarine_View, Game_Screen],
+	scene: [Container_Scene,Menu, Submarine_View, Game_Screen],
 };
 
 new Phaser.Game(config);
